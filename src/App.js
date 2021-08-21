d4c5b0430b6915f7215116dd59e39ecd970dc7c8
kimts2627 <kimtstststs@gmail.com>
2021-08-21 14:52:45 +0900

    :sparkles: FEAT: add new apollo client
    add apollo client, and request data by useQuery hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { useQuery, gql } from '@apollo/client';
 import './App.css';
 
+const EXCHANGE_RATES = gql`
+	query GetExchangeRates {
+		rates(currency: "USD") {
+			currency
+			rate
+		}
+	}
+`;
+
 function App() {
-	return <div className="App"></div>;
+	const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+	if (loading)
+		return (
+			<div className="App">
+				<p>Loading...</p>
+			</div>
+		);
+	if (error)
+		return (
+			<div className="App">
+				<p>Error :(</p>
+			</div>
+		);
+	return (
+		<div className="App">
+			{data.rates.map(({ currency, rate }) => (
+				<div key={currency}>
+					<p>
+						{currency}: {rate}
+					</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	useQuery,
+	gql,
+} from '@apollo/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+	uri: 'https://48p1r2roz4.sse.codesandbox.io',
+	cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+);
